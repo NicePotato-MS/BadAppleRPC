c186ad857dd58d3b89a7d6776c8c48e62759d55f
NicePotato-MS <micahsuessgithub@gmail.com>
2024-03-09 23:31:36 -0600

    1 seconf to 15 second

--- makegifs.py
+++ makegifs.py
@@ -1,8 +1,9 @@
 import moviepy.editor as mp
 import imageio
 import os
+import math
 
-def split_video_to_gifs(input_video, output_folder, gif_fps=10):
+def split_video_to_gifs(input_video, output_folder, gif_fps=30,clip_duration=5):
     # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -14,9 +15,9 @@ def split_video_to_gifs(input_video, output_folder, gif_fps=10):
     duration = int(video_clip.duration)
 
     # Split the video into 1-second-long clips
-    for i in range(duration):
+    for i in range(0, duration, clip_duration):
         start_time = i
-        end_time = i + 1
+        end_time = min(i + clip_duration, duration)
 
         # Create a subclip from the original video
         subclip = video_clip.subclip(start_time, end_time)
@@ -25,7 +26,7 @@ def split_video_to_gifs(input_video, output_folder, gif_fps=10):
         frames = [frame for frame in subclip.iter_frames(fps=gif_fps)]
 
         # Save the frames as a GIF in the output folder
-        gif_filename = os.path.join(output_folder, f"{i}.gif")
+        gif_filename = os.path.join(output_folder, f"{math.ceil(i/clip_duration)}.gif")
         imageio.mimsave(gif_filename, frames, fps=gif_fps)
 
     # Close the video clip
@@ -35,5 +36,6 @@ if __name__ == "__main__":
     input_video_path = 'badapple-square.mp4'
     output_folder_path = 'clip'
     gif_framerate = 30
+    clip_duration = 15
 
-    split_video_to_gifs(input_video_path, output_folder_path, gif_fps=gif_framerate)
+    split_video_to_gifs(input_video_path, output_folder_path, gif_fps=gif_framerate,clip_duration=clip_duration)
--- rpc.py
+++ rpc.py
@@ -6,14 +6,17 @@ client_id = '1216241049371148369'
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 
-clip = 1
-total_clips = 218
+clip = 0
+total_clips = 14
 
 while True:  # The presence will stay on as long as the program is running
-    if clip == total_clips:
-        clip = 1
+    if clip == total_clips+1:
+        clip = 0
     print(RPC.update(state="Made by NicePotato",
-                     details="Clip: "+str(clip)+"/"+str(total_clips),
-                     large_image=""))
+                     details=f"Clip: {clip+1}/{total_clips}",
+                     large_image=f"https://raw.githubusercontent.com/NicePotato-MS/BadAppleRPC/main/clip/{clip}.gif"))
     clip=clip+1
-    time.sleep(1)
+    if clip == 14:
+        time.sleep(8.28)
+    else:
+        time.sleep(15)
